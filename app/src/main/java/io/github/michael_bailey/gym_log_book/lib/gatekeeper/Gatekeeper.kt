9c0a8f1beda55d68bcda50f9b595768ec945dec7
michael-bailey <mickyb18a@gmail.com>
2023-05-06 20:01:39 +0100

    fixed gatekeeper saving state

--- app/src/main/java/io/github/michael_bailey/gym_log_book/lib/gatekeeper/Gatekeeper.kt
+++ app/src/main/java/io/github/michael_bailey/gym_log_book/lib/gatekeeper/Gatekeeper.kt
@@ -3,6 +3,7 @@ package io.github.michael_bailey.gym_log_book.lib.gatekeeper
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.github.michael_bailey.gym_log_book.App
+import io.github.michael_bailey.gym_log_book.extension.application.preferences
 
 /**
  * a gatekeeper manager to handle semi dynamic kill-switches
@@ -18,8 +19,12 @@ object Gatekeeper {
 		"database_weight_item_view" to false,
 	)
 
+	/**
+	 * source of truth for all gatekeeper state
+	 * it is loaded on boot
+	 */
 	private val gatekeeperMap: MutableMap<String, MutableLiveData<Boolean>> =
-		getDefaults()
+		loadGatekeepers()
 
 	/* functions to fetch gatekeepers */
 	fun eval(name: String): Boolean = application.appDebugPreferencesManager
@@ -29,17 +34,23 @@ object Gatekeeper {
 
 	fun setGatekeeper(name: String, value: Boolean) {
 		gatekeeperMap[name]?.value = value
+		application.preferences().edit().putBoolean("gk_$name", value).commit()
 	}
 
-	fun getGatekeeperNames(): List<String> = this.gatekeeperMap.keys.toList()
+	private fun getDefaults(): Map<String, Boolean> = gatekeeperList.toMap()
 
-	private fun getDefaults() = gatekeeperList
-		.map { it.first to MutableLiveData(it.second) }
-		.toMap() as MutableMap<String, MutableLiveData<Boolean>>
+	fun loadGatekeepers(): MutableMap<String, MutableLiveData<Boolean>> =
+		getDefaults()
+			.map {
+				it.key to MutableLiveData(
+					application.preferences().getBoolean("gk_${it.key}", it.value)
+				)
+			}.toMap() as MutableMap<String, MutableLiveData<Boolean>>
+
 
 	fun reset() {
 		getDefaults().forEach {
-			setGatekeeper(it.key, it.value.value!!)
+			setGatekeeper(it.key, it.value)
 		}
 	}
 
