77cde5a7f5395db624d100ffee310e53672c50e6
michael-bailey <bailey-michael1@outlook.com>
2026-04-29 17:55:20 +0100

    created koin module and added plugins to application setup

--- gym_log_book_server/src/main/kotlin/Application.kt
+++ gym_log_book_server/src/main/kotlin/Application.kt
@@ -1,22 +1,32 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package net.michael_bailey.gym_log_book.server
 
+import com.auth0.jwt.JWTVerifier
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
 import kotlinx.rpc.krpc.ktor.server.Krpc
+import kotlinx.rpc.krpc.ktor.server.KrpcRoute
 import kotlinx.rpc.krpc.ktor.server.rpc
 import kotlinx.rpc.krpc.serialization.json.json
+import net.michael_bailey.gym_log_book.server.authentication.scope.ViewerScope
+import net.michael_bailey.gym_log_book.shared.authentication.controller.AuthenticationController
 import net.michael_bailey.gym_log_book.shared.counter.controller.CounterController
 import net.michael_bailey.gym_log_book.shared.exercise.controller.ExerciseEntryController
 import net.michael_bailey.gym_log_book.shared.exercise.controller.ExerciseTypeController
 import org.koin.core.annotation.KoinApplication
-import org.koin.ktor.ext.get
+import org.koin.core.qualifier.named
+import org.koin.core.scope.Scope
 import org.koin.ktor.plugin.Koin
+import org.koin.ktor.plugin.koin
+import org.koin.mp.KoinPlatform.getKoin
 import org.koin.plugin.module.dsl.withConfiguration
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @KoinApplication
 class ApplicationContainer
@@ -34,6 +44,23 @@ fun Application.module() {
 		withConfiguration<ApplicationContainer>()
 	}
 
+	val verifier: JWTVerifier = this.koin().get()
+
+	install(Authentication) {
+		jwt {
+			realm = "Gym Log Book"
+			verifier(verifier)
+			validate { credential ->
+				// Ensure the token has a valid subject (userId)
+				if (credential.payload.subject != null) {
+					JWTPrincipal(credential.payload)
+				} else {
+					null
+				}
+			}
+		}
+	}
+
 	install(Krpc) {
 		serialization {
 			json()
@@ -45,18 +72,46 @@ fun Application.module() {
 			call.respondText("ok")
 		}
 
-		rpc("/rpc") {
-			registerService<CounterController> {
-				get()
+		rpc("/rpc/authentication") {
+			val scope = createScope()
+			registerService<AuthenticationController> {
+				scope.get()
 			}
-			registerService<ExerciseTypeController> {
-				get()
-			}
-			registerService<ExerciseEntryController> {
-				get()
+		}
+
+		authenticate {
+			rpc("/rpc") {
+
+				val scope = createScope()
+
+				registerService<CounterController> {
+					scope.get()
+				}
+				registerService<ExerciseTypeController> {
+					scope.get()
+				}
+				registerService<ExerciseEntryController> {
+					scope.get()
+				}
+
+				rpcConfig { serialization { json() } }
+
+				this.coroutineContext.job.invokeOnCompletion {
+					scope.close()
+				}
 			}
 		}
 	}
 
 	println("Started application module")
+}
+
+fun KrpcRoute.createScope(): Scope {
+	val scope = getKoin().createScope(
+		scopeId = "connection-${Uuid.generateV4()}",
+		qualifier = named<ViewerScope>(),
+	)
+	scope.declare(call)
+
+	return scope
 }
---
+++ gym_log_book_server/src/main/kotlin/authentication/AuthenticationModule.kt
@@ -0,0 +1,38 @@
+package net.michael_bailey.gym_log_book.server.authentication
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import net.michael_bailey.gym_log_book.server.authentication.config.IJWTConfiguration
+import net.michael_bailey.gym_log_book.server.authentication.factory.ViewerContextFactory
+import net.michael_bailey.gym_log_book.server.authentication.model.ViewerContext
+import net.michael_bailey.gym_log_book.server.authentication.scope.ViewerScope
+import org.koin.core.annotation.*
+
+@Module
+@ComponentScan("net.michael_bailey.gym_log_book.server.authentication")
+@Configuration
+class AuthenticationModule {
+
+	@Single
+	fun jwtVerifier(
+		authenticationConfiguration: IJWTConfiguration
+	): JWTVerifier = JWT.require(Algorithm.HMAC256(authenticationConfiguration.jwtSecret))
+		.withIssuer(authenticationConfiguration.issuer)
+		.withAudience(authenticationConfiguration.audience)
+		.build()
+
+	@Single
+	@Qualifier(Map::class, "environment")
+	fun environmentMap(): Map<String, String> = System.getenv()
+
+	@Single
+	fun coroutineScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+
+	@Scoped
+	@Scope(ViewerScope::class)
+	fun viewerContext(factory: ViewerContextFactory): ViewerContext = factory.create()
+}
