ce70eff74aef35548fbb05e7ec4eeab7502b3dbc
michael-bailey <bailey-michael1@outlook.com>
2026-05-12 20:07:26 +0100

    added basic create function to exercise entry service

--- gym_log_book_client/src/commonMain/kotlin/exercise/service/ExerciseEntryService.kt
+++ gym_log_book_client/src/commonMain/kotlin/exercise/service/ExerciseEntryService.kt
@@ -1,10 +1,27 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package net.michael_bailey.gym_log_book.client.exercise.service
 
 import net.michael_bailey.gym_log_book.shared.exercise.controller.ExerciseEntryController
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 class ExerciseEntryService(
 	private val exerciseEntryController: ExerciseEntryController
 ) {
+	suspend fun createNewExerciseEntry(
+		exerciseTypeId: Uuid,
+		entrySetNumber: Int,
+		entryWeight: Double,
+		entryReps: Int,
+	) {
+		exerciseEntryController.createEntry(
+			exerciseTypeId = exerciseTypeId,
+			entrySetNumber = entrySetNumber,
+			entryWeight = entryWeight,
+			entryReps = entryReps,
+		)
+	}
 
 	val allEntries = exerciseEntryController.getExerciseEntries()
 
