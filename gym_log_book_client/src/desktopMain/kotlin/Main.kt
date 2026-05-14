7f5cf63bf0c27d458026f772f2bc3d45172f85ca
michael-bailey <bailey-michael1@outlook.com>
2026-05-14 07:42:00 +0100

    fix(lint): removed unused variable

--- gym_log_book_client/src/desktopMain/kotlin/Main.kt
+++ gym_log_book_client/src/desktopMain/kotlin/Main.kt
@@ -18,7 +18,7 @@ import org.koin.compose.koinInject
 import org.koin.core.context.startKoin
 
 fun main() = application {
-	val koin = startKoin {
+	startKoin {
 		modules(
 			platformModule,
 			applicationModule,
