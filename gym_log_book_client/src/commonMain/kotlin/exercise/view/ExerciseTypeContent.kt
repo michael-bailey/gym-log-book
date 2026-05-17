e7bdfee675bd223cd055a020ec909ee0fed832de
michael-bailey <bailey-michael1@outlook.com>
2026-05-18 00:02:57 +0100

    create(feat): Created new type overview content and cards

--- gym_log_book_client/src/commonMain/kotlin/exercise/view/ExerciseTypeContent.kt
+++ gym_log_book_client/src/commonMain/kotlin/exercise/view/ExerciseTypeContent.kt
@@ -1,46 +1,82 @@
 package net.michael_bailey.gym_log_book.client.exercise.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Start
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 @Composable
 fun ExerciseTypeContent(
-	modifier: Modifier = Modifier,
-	name: String,
-	equipmentClass: String,
-	isUsingUserWeight: Boolean
+	type: String,
+	createDate: String,
+	createdTime: String,
+	setNumber: Int,
+	weight: Double,
+	reps: Int
 ) {
 	Column(
-		modifier = modifier
+		Modifier
+			.fillMaxWidth()
+			.wrapContentHeight()
+			.padding(16.dp)
 	) {
 		Row(
-			modifier = Modifier.fillMaxWidth()
+			Modifier
 				.wrapContentHeight()
-				.padding(16.dp),
-			verticalAlignment = Alignment.CenterVertically,
+				.fillMaxWidth(),
+			horizontalArrangement = Arrangement.SpaceBetween
 		) {
-			Icon(
-				modifier = Modifier.padding(end = 12.dp),
-				imageVector = Icons.Default.Start,
-				contentDescription = ""
+			Text(
+				text = type,
+				fontSize = 14.sp,
+				fontWeight = FontWeight(400)
 			)
-			Column {
+			Row(
+				Modifier
+					.wrapContentHeight()
+					.wrapContentWidth(),
+				horizontalArrangement = Arrangement.spacedBy(10.dp)
+			) {
 				Text(
-					text = name,
-					fontSize = 18.sp
+					text = createDate,
+					fontSize = 14.sp,
+					fontWeight = FontWeight(400)
 				)
-				Text(text = "$equipmentClass class")
-				Text(text = "Uses user weight: $isUsingUserWeight")
+				Text(
+					text = createdTime,
+					fontSize = 14.sp,
+					fontWeight = FontWeight(400)
+				)
 			}
+		}
+		Row(
+			Modifier
+				.wrapContentHeight()
+				.fillMaxWidth(),
+			horizontalArrangement = Arrangement.SpaceBetween
+		) {
+			Text(
+				text = "Set: $setNumber",
+				fontSize = 22.sp,
+				fontWeight = FontWeight(500)
+			)
+			Text(
+				text = "$weight KG",
+				fontSize = 22.sp,
+				fontWeight = FontWeight(500)
+			)
+			Text(
+				text = "$reps reps",
+				fontSize = 22.sp,
+				fontWeight = FontWeight(500)
+			)
 		}
 	}
 }
@@ -48,10 +84,17 @@ fun ExerciseTypeContent(
 @Preview
 @Composable
 fun ExerciseTypeContent_Preview() {
-	ExerciseTypeContent(
-		modifier = Modifier.fillMaxWidth(),
-		name = "Type",
-		equipmentClass = "Free weight",
-		isUsingUserWeight = false
+
+	val now = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+	val time = now.time.toString()
+	val date = now.date.toString()
+
+	ExerciseEntryContent(
+		type = "Test Type",
+		createDate = date,
+		createdTime = time,
+		setNumber = 3,
+		weight = 12.25,
+		reps = 8
 	)
 }
