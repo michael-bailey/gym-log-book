bf4597b2fe43a975cbce41fc1d30cdb7a844e244
michael-bailey <bailey-michael1@outlook.com>
2026-05-31 08:50:03 +0100

    create(feat): Created database migration manager

---
+++ gym_log_book_server/src/main/kotlin/database/schema/SchemaVersionTable.kt
@@ -0,0 +1,12 @@
+package net.michael_bailey.gym_log_book.server.database.schema
+
+import org.jetbrains.exposed.v1.core.dao.id.UuidTable
+import kotlin.time.Clock
+
+class SchemaVersionTable(
+	private val clock: Clock
+) : UuidTable("schema_version") {
+	val version = double("version")
+	val appliedAt = long("applied_at").default(clock.now().toEpochMilliseconds())
+	val description = varchar("description", 255)
+}
